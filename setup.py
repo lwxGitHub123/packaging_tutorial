19b02dc62597bffb1ee3a34896c8b56d034bd7f0
lwxGitHub123 <34017587+lwxGitHub123@users.noreply.github.com>
2021-11-05 17:30:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setuptools.setup(
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/lwxGitHub123/packaging_tutorial",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/lwxGitHub123/packaging_tutorial",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
