70c1771994db706b942df43625c9ae7f19cd95b5
lwxGitHub123 <34017587+lwxGitHub123@users.noreply.github.com>
2021-11-05 17:37:01 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE",
+    name="example",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
